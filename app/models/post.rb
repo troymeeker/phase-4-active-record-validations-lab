70de28095a4662710527a72710813ede81b17bdc
troymeeker <troymeeker1@gmail.com>
2022-02-11 11:39:50 -0800

    commit tree 5a402562829c1ccc3104e597bf714e4db90958e0

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,20 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+    validate :clickbait
+
+  PATTERN = [
+    /Won't Believe/i,
+    /Secret/i,
+    /Top \d/i,
+    /Guess/i
+  ]
+
+  def clickbait
+    if PATTERN.none? { |pat| pat.match title }
+      errors.add(:title, "must be more clickbait")
+    end
+  end
 end
